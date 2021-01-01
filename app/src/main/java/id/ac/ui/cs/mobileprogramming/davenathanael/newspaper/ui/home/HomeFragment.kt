2ba2aa36a29c7b832ea966d20bee88fbc0731a19
Dave Nathanael <--global>
2021-01-01 19:27:18 +0700

    feat: add connectivity checking capability

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/davenathanael/newspaper/ui/home/HomeFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/davenathanael/newspaper/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.mobileprogramming.davenathanael.newspaper.ui.home
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import com.google.android.material.snackbar.Snackbar
 import id.ac.ui.cs.mobileprogramming.davenathanael.newspaper.NewspaperApplication
 import id.ac.ui.cs.mobileprogramming.davenathanael.newspaper.adapter.StoryListAdapter
 import id.ac.ui.cs.mobileprogramming.davenathanael.newspaper.databinding.FragmentHomeBinding
@@ -31,6 +35,27 @@ class HomeFragment : Fragment() {
 
         subscribeUi(adapter)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cm = requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+        if (isConnected) {
+            Snackbar.make(
+                binding.root,
+                "Connected to internet! Fetching latest stories...",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        } else {
+            Snackbar.make(
+                binding.root,
+                "Can't fetch latest stories! Please make sure you're connected",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 
     private fun subscribeUi(adapter: StoryListAdapter) {
